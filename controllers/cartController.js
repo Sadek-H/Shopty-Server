1acd8cf70ee19c4655093d378472c657d58d4a7d
Sadek-H <sadek111st@gmail.com>
2026-03-07 01:25:07 +0600

    cart manage

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,7 +3,7 @@ const  createCart = async (req,res)=>{
     try{
         const data = req.body;
         const {id} = req.params;
-        const alreadycart = await cart.findOne({cart})
+        const alreadycart = await cart.findOne({id})
         if(alreadycart){
             return res.status(400).json({success: false, message: 'Cart already exists'});
         }
