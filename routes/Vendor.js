9f6d3b69ff3b3e18487d3d37dc015dfabae819ff
Sadek-H <sadek111st@gmail.com>
2026-03-10 03:26:18 +0600

    vendor manage

--- routes/Vendor.js
+++ routes/Vendor.js
@@ -1,6 +1,6 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const vendorController = require("../controllers/vendorController");
 
-router.post("/becomevendors", vendorController)
+router.post("/becomevendors", vendorController);
 
 module.exports = router;
