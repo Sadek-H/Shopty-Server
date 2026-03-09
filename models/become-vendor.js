75a5231ebef2e6d601e9709f6f61007761fcd818
Sadek-H <sadek111st@gmail.com>
2026-03-10 03:09:53 +0600

    vendor manage

--- models/become-vendor.js
+++ models/become-vendor.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 
 const BecomevendorSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true},
-    phone:{ type: String, required: true},
-    Shopname:{ type: String, required: true},
-    category: { type: String, required: true},
     address: { type: String, required: true},
-    License: { type: String, required: true},
+    category: { type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    fullName: {type: String, required: true},
+    license: { type: String, required: true},
+    phone:{ type: String, required: true},
+    shopName:{ type: String, required: true},
     status: { type: String, default: "pending" }
 })
 
