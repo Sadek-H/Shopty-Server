2d5bdda4948cb018c29678360c962b567774d9e7
Sadek-H <sadek111st@gmail.com>
2026-03-12 01:53:32 +0600

    aligned

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,45 +1,46 @@
-const cart = require('../models/Cartmodel');
-const  createCart = async (req,res)=>{
-    try{
-        const data = req.body;
-        const {id} = req.params;
-        const alreadycart = await cart.findOne(id)
-        if(alreadycart){
-            return res.status(400).json({success: false, message: 'Cart already exists'});
-        }
-        const carts = new cart(data);
-        await carts.save();
-        res.status(201).json({success: true, carts});
+const cart = require("../models/Cartmodel");
+const createCart = async (req, res) => {
+  try {
+    const data = req.body;
+    const { id } = req.params;
+    const alreadycart = await cart.findOne(id);
+    if (alreadycart) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Cart already exists" });
     }
-    catch (error){
-        res.status(500).json({success:false, message: error.message});
-    }
-}
+    const carts = new cart(data);
+    await carts.save();
+    res.status(201).json({ success: true, carts });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
-const getCart = async (req,res)=>{
-    try{
-        const carts = await cart.find();
-        res.status (200).json({success: true, carts});
+const getCart = async (req, res) => {
+  try {
+    const carts = await cart.find();
+    res.status(200).json({ success: true, carts });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
+const deleteCart = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedcart = await cart.findByIdAndDelete(id);
+    if (!deletedcart) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Cart not found" });
     }
-    catch(error){
-        res.status(500).json({success:false, message: error.message});
-    }
-}
+    res
+      .status(200)
+      .json({ success: true, message: "Cart deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
-const deleteCart = async (req,res)=>{
-    try{
-        const {id} = req.params;
-        const deletedcart = await cart.findByIdAndDelete(id);
-        if(!deletedcart){
-            return res.status(404).json({success:false, message: 'Cart not found'});
-        }
-        res.status(200).json({success: true, message: 'Cart deleted successfully'});
-    }
-    catch(error){
-        res.status(500).json({success:false, message: error.message});
-    }
-}
-
-module.exports = {createCart, getCart, deleteCart};
-
+module.exports = { createCart, getCart, deleteCart };
