abfa5e59c43e0d0846a98a67a1268496b16f3aa3
Sadek-H <sadek111st@gmail.com>
2026-03-13 03:15:02 +0600

    try to solve err

--- Config/cloudinary.js
+++ Config/cloudinary.js
@@ -1,21 +1,19 @@
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
 const cloudinary = require('cloudinary').v2;
-
+const  {CloudinaryStorage}  = require('multer-storage-cloudinary');
 
 cloudinary.config({
-     cloud_name: 'dqajerl82',
-  api_key: '932913874974494',
-  api_secret: 'hYT179n0qZ8MlZnejJCz4_9xU1U'
-})
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
 
-const storage = new CloudinaryStorage({
-    cloudinary,
-    params:{
-        folder: 'Shopty',
-        format: async ()=> 'png',
-        public_id: (req,file)=> Date.now()+ ''+ file.originalname,
-    }
+const storage = new createCloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'Shopty',
+    allowed_formats: ['jpg', 'jpeg', 'png'],
+    public_id: (req, file) => Date.now() + '-' + file.originalname,
+  },
+});
 
-})
-module.exports = {cloudinary, storage};
+module.exports = { cloudinary, storage };
